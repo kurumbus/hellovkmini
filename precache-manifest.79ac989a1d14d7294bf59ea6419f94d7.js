afda8da93ecf488f2772908028e8ba059e5373ff
kurumbus <grigory.shein@gmail.com>
2020-05-24 12:52:42 +0700

    Updates

---
+++ precache-manifest.79ac989a1d14d7294bf59ea6419f94d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40115365ffb7f2ac78f57f6b0a318ab",
+    "url": "/hellovkmini/index.html"
+  },
+  {
+    "revision": "14e23cc50cb890f38407",
+    "url": "/hellovkmini/static/css/2.a28d9ceb.chunk.css"
+  },
+  {
+    "revision": "14e23cc50cb890f38407",
+    "url": "/hellovkmini/static/js/2.3cca5f2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hellovkmini/static/js/2.3cca5f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174f4c032879c6af8635",
+    "url": "/hellovkmini/static/js/main.562605ef.chunk.js"
+  },
+  {
+    "revision": "a3617c9b76182b0d1fa6",
+    "url": "/hellovkmini/static/js/runtime-main.75d19bb2.js"
+  }
+]);
