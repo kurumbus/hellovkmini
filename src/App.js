50ad3f252b4e97cdb75b23296ec334a82792ca48
kurumbus <grigory.shein@gmail.com>
2020-05-24 21:31:21 +0700

    add map wip

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
     Avatar,
     Banner,
     Button,
+    Card,
     Div,
     Gallery,
     Group,
@@ -20,6 +21,8 @@ import {
 import '@vkontakte/vkui/dist/vkui.css';
 import Dropzone from "react-dropzone";
 import request from "superagent";
+import L from "leaflet"
+require('./vendor/leaflet-openweathermap.js');
 
 class App extends Component {
 
@@ -35,6 +38,7 @@ class App extends Component {
             webEntities: [],
             showSpinner: false,
             visuallySimilarImages: [],
+            landmarks: [],
             slideIndex: 0,
         };
     }
@@ -44,7 +48,7 @@ class App extends Component {
         return function (files) {
             _this.setState({
                 showSpinner: true, pagesWithMatchingImages: [], webEntities: [],
-                visuallySimilarImages: [], slideIndex: 0,
+                visuallySimilarImages: [], slideIndex: 0, landmarks: [],
             });
             const req = request.post('https://ocr.kurumbus.com/api/web');
             files.forEach(file => {
@@ -56,6 +60,7 @@ class App extends Component {
                 _this.setState({
                     pagesWithMatchingImages: res.body.pages_with_matching_images ?? [],
                     webEntities: res.body.web_entities ?? [],
+                    landmarks: res.body.landmarks ?? [],
                     visuallySimilarImages: res.body.visually_similar_images ?? [],
                     showSpinner: false,
                 });
@@ -121,6 +126,38 @@ class App extends Component {
                                         this.state.webEntities.join(', ')
                                     }
                                 </Div>
+                            </Group>
+                        }
+                        {
+                            this.state.landmarks.length > 0 &&
+                            <Group header={<Header mode="secondary">Места на фотографии:</Header>}>
+                                {
+                                    this.state.landmarks.map((landmark, i) => {
+                                       // let zoom = this.zoom ? this.zoom : 10;
+                                      //  let myMap = L.map("map").setView([landmark.locations[0].latitude, landmark.locations[0].longitude], zoom);
+                                        /*L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                                            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                                            maxZoom: 18,
+                                            id: 'mapbox/streets-v11',
+                                            tileSize: 512,
+                                            zoomOffset: -1,
+                                            accessToken: "pk.eyJ1Ijoia3VydW1idXMiLCJhIjoiY2s4NWplbWgyMDdpdTNldnBrcTF3a2Q0aCJ9.l0FPgUKy2gpn9MAEvk5eiA"
+                                        }).addTo(myMap);*/
+
+                                        return (
+                                            <Div style={{marginVertical: 20}}>
+                                                <Card>
+                                                    <Div>{landmark.title}</Div>
+                                                    { landmark.locations.length > 0 &&
+                                                    <Div id={'map'+i}>
+
+                                                    </Div>
+                                                    }
+                                                </Card>
+                                            </Div>
+                                        )
+                                    })
+                                }
                             </Group>
                         }
                         {
